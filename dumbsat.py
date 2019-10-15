db7ee8a6b991810c8415c4a196e87fcadd47e687
Bart Massey <bart@cs.pdx.edu>
2019-10-14 23:26:42 -0700

    removed dead code

--- dumbsat.py
+++ dumbsat.py
@@ -4,19 +4,6 @@ import sys
 
 from formula import *
 
-def random_formula(nvars, nclauses):
-    vars = [Var("A" + str(name)) for name in range(nvars)]
-    clauses = []
-    for _ in range(nclauses):
-        clause = []
-        for _ in range(3):
-            v = random.choice(vars)
-            s = bool(random.randrange(2))
-            clause.append(Lit(v, s))
-        clauses.append(Clause(clause))
-    return Formula(vars, clauses)
-
-
 def dpll(f):
     for v in f.vars:
         if v.value == None:
